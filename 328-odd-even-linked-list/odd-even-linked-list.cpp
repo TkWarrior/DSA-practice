fd263301866fe5bf788f8c19aa6aad842ac9fd9f
TkWarrior <147327168+TkWarrior@users.noreply.github.com>
2025-03-24 14:46:57 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (8.67%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -11,9 +11,9 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
+        //Optimal Solution using O(1) space complexity
         ListNode* odd = head;
        
-        
         if(head==NULL || head->next ==NULL){
             return head;
         }
@@ -29,5 +29,41 @@ public:
         odd->next = evenHead;
         
         return head;
+
+        /*
+        Brute Force Approach using O(N) Space complexity and O(N) time complextiy
+        ListNode* temp = head;
+        vector<int>arr;
+
+        //Edge cases
+        if(head==NULL || head->next ==NULL){
+            return head;
+        }
+        //first we will store the add index linked list to the array it is the 1 - based indexing
+        while(temp!=NULL && temp->next!=NULL){
+            arr.push_back(temp->val);
+            temp = temp->next->next;
+        }
+
+        // this is the condition when temp!=NULL but temp->next = NULL iss case mein fir hum array mein wo last element push kar denge
+        if(temp){
+            arr.push_back(temp->val);
+        }
+        //after that we will store the even index linked list to the array
+        temp = head->next;
+        while(temp!=NULL && temp->next!=NULL){
+            arr.push_back(temp->val);
+            temp = temp->next->next;
+        }
+        if(temp){
+            arr.push_back(temp->val);
+        }
+        
+        temp = head;
+        for(int i = 0 ; i<arr.size() ; i++){
+            temp->val = arr[i];
+            temp = temp->next;
+        }
+        return head;*/
     }
 };
