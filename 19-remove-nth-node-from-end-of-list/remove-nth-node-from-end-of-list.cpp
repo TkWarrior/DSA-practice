9965c02770041e0fede1dea75c36ac86d5cf0c71
TkWarrior <147327168+TkWarrior@users.noreply.github.com>
2025-03-24 16:50:36 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (59.79%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -13,20 +13,27 @@ public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         ListNode* l1 = head;
         int count = 0;
-        
+       
+       //Edge cases
         if(head==NULL || head->next==NULL){
             return NULL;
         }
         
+        //we will traverse the linked list and will count the no of nodes
        while(l1!=NULL ){
             count++;
             l1 = l1->next;
             
        }
         
+        // if the number of nodes is equal to element's position which has to be deleted
+        // it means it is pointed towards head of the linked list
         if(count==n){
+            //we will Node pointer to the head->next
             ListNode* newHead = head->next;
-            delete head;
+            //then we will delete the head
+            // delete head;
+            // will return the new head
             return newHead;
         }
       
@@ -45,7 +52,7 @@ public:
              
        
          l1->next = deleteNode->next;
-         delete deleteNode;
+        //  delete deleteNode;
         }
        
         return head;
