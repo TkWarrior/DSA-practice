02a839fedf4c9c252c0f0c1d5a23d1b10beb1e1e
TkWarrior <147327168+TkWarrior@users.noreply.github.com>
2025-04-04 23:40:25 +0530

    Time: 0 ms (100.00%) | Memory: 95.1 MB (63.30%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,39 +1,56 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2){
+        
+        //Brute force - time complexity O(n) space complexity O(n)
         int n1 = nums1.size();
         int n2 = nums2.size();
-        vector<int>ans;
-        int i = 0 , j = 0 , k=0;
-        //mergin 
+        int n = n1+n2;
+        //better approach reduce the space complexity to O(1)
+        int count=0;
+        int index1 = n/2 - 1;
+        int index2 = n/2;
+        int index1ele = -1;
+        int index2ele = -1;
+        int i = 0 , j = 0 ;
+        //merging 
         while(i<n1 && j<n2){
             if(nums1[i]<nums2[j]){
-                ans.push_back(nums1[i++]) ;
-                
+                if(count == index1) index1ele = nums1[i];
+                if(count == index2) index2ele = nums1[i];
+                count++;
+                i++;             
             }
             else{
-                ans.push_back(nums2[j++]);
-               
+                if(count == index1) index1ele = nums2[j];
+                if(count == index2) index2ele = nums2[j];
+                count++;
+                j++;
             }
         }
         while(i<n1){
-            ans.push_back(nums1[i++]);
+            if(count == index1) index1ele = nums1[i];
+            if(count == index2) index2ele = nums1[i];
+            count++;
+            i++;
            
         }
         while(j<n2){
-            ans.push_back(nums2[j++]);
+            if(count == index1) index1ele = nums2[j];
+            if(count == index2) index2ele = nums2[j];
+            count++;
+            j++;
            
         }
         
         // now we will apply binary search here
         
-        double mid = (n1+n2)/2;
         double median;
         if((n1+n2)%2==0){
-            return double((double)ans[mid]+(double)ans[mid-1])/2;
+            return double((double)index1ele + (double)index2ele)/2;
         }
         else{
-             median = ans[mid];
+             median = (double) index2ele;
         }
         return median;
 
