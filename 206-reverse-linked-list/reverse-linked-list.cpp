46e08ad604cfe2baec462bb5634b95b7532c993f
TkWarrior <147327168+TkWarrior@users.noreply.github.com>
2025-04-09 17:07:42 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (9.81%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -11,51 +11,24 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        //brute force - time complexity = O(n)
-        // space complexity = O(n)
-        // vector<int>arr;
-        // ListNode*l1 = head;
-        // while(l1!=NULL){
-        //     arr.push_back(l1->val);
-        //     l1 = l1->next;
-        // }
-        // int n = arr.size();
-        // for(int i = 0 ; i<n/2 ; i++){
-        //     swap(arr[i],arr[n-i-1]);
-        // }
-        // l1 = head;
-        // for(int i = 0 ; i<n ; i++){
-        //     l1->val = arr[i];
-        //     l1 = l1->next;
-        // }
-        // return head;
+        ListNode*l1 = head;
+        vector<int>ans;
 
-        // optimal soln - 1 - O(n) time complexity and O(1) space complexity
-        // ListNode* temp = head;
-        
-        // ListNode* prev = NULL;
-        // while(temp!=NULL ){
-        //     ListNode* front = temp->next;
-        //     temp->next = prev;
-            
-        //     prev = temp;
-            
-        //     temp = front;
-        // }
-        
-        // return prev;
-
-        //optimal soln -2 using recursion
-         //base case - if there is any empty node or any single node present 
-        if(head==NULL || head->next==NULL){
+        if(l1 == NULL || l1->next == NULL){
             return head;
         }
-        ListNode*newHead = reverseList(head->next);
-        ListNode*front = head->next;
-        front->next = head;
-        head->next = NULL;
-
-        return newHead;
-         
+        while(l1!=NULL){
+            
+            ans.push_back(l1->val);
+            l1 = l1->next;
+        }
+        l1 = head;
+        for(int i = ans.size()-1 ; i>=0 ; i--){
+           
+            l1->val = ans[i];
+            l1 = l1->next;
+            
+        }
+        return head;
     }
 };
