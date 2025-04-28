ddc3e96958216293e9c0e7fc33dcdc218184e539
TkWarrior <147327168+TkWarrior@users.noreply.github.com>
2025-04-29 04:10:14 +0530

    Time: 0 ms (100.00%) | Memory: 55.1 MB (22.91%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -2,18 +2,20 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         int n = nums.size();
-        vector<bool> hash(n+1,false);
-        
+        vector<bool>hash(n+1,false);
+        // for each loop
         for(int num:nums){
-            if(num>0 && num<=n)  hash[num] = true;
-           
+            if(num>0 && num<=n) hash[num] = true;
         }
 
-        for(int i = 1 ; i<=n ;i++){
+        // now we will use another for loop to find smallest +ve integer in 
+        for(int i=1 ; i<=n ; i++){
             if(!hash[i]){
-                return i ;
+                return i;
             }
         }
+        // if hash array contain all true values from 1 to n then 
+
         return n+1;
     }
 };
