460b18be2e3ea6f0a7cef2426620f1c89b2b2f7a
TkWarrior <147327168+TkWarrior@users.noreply.github.com>
2025-05-07 16:09:38 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (55.61%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    // typedef pair<char,int>P;
+    typedef pair<char,int>P;
     string frequencySort(string s) {
         // learnt about lambda - in  this problem it is used for custom sorting 
         // vector mein pair bhi store kara sakte hai like in this case we have used the pair of character and its frequency inside the vector
         // vector<pair<char,int>>>vec(123)
-        vector<pair<char,int>>vec(123);
+        vector<P>vec(123);
         string result = "";
         for(char &ch : s){
             int freq = vec[ch].second;
             vec[ch] = {ch,freq+1};
         }
 
-        auto lambda = [&](pair<char,int> & p1,pair<char,int> & p2){
+        auto lambda = [&](P & p1,P & p2){
             return p1.second>p2.second;
         };
         sort(vec.begin(),vec.end(),lambda);
