561bed3b69c7ca0f3b270f5d566fc7d22a7a180e
TkWarrior <147327168+TkWarrior@users.noreply.github.com>
2025-05-30 19:54:53 +0530

    Time: 102 ms (7.48%) | Memory: 27.9 MB (94.81%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        // vector<int>temp;
+        // unordered_map<int,int>umap;
+        // int n = nums.size();
+        // int new_size = 2*n;
+        // for(int i = 0 ; i<new_size-1 ; i++){
+        //     int j = (n+i)%n;
+        //     temp.push_back(nums[j]);
+        // }
+        // stack<int>st;
+        // vector<int>ans;
+        // for(int j = temp.size()-1 ; j>=0 ; j--){
+        //     while(!st.empty() && st.top()<=temp[j]){
+        //         st.pop();
+        //     }
+        //     if(st.empty()){
+        //         umap[temp[j]] = -1;
+        //     } 
+        //     else{
+        //         if(umap.find(temp[j])==umap.end() && umap[temp[j]]<=st.top()){
+        //              umap[temp[j]] = st.top();
+        //         }
+        //         else{
+        //             umap[temp[j]] = st.top();
+        //         }
+        //     }
+        //     st.push(temp[j]);
+        // }
+        // for(int i = 0 ; i<n ; i++){
+        //    ans.push_back(umap[nums[i]]);
+        // }
+        // return ans;
+
+        //brute force approach
+        int n = nums.size();
+        vector<int>nge(n,-1);
+         
+         for(int i = 0 ; i<n ; i++){
+            for(int j = i+1 ; j<i+n ; j++){
+                int idx = (j%n);
+                if(nums[idx]>nums[i]){
+                    nge[i] = nums[idx];
+                    break;
+                }
+            }
+         }
+         return nge;
+    }
+};
