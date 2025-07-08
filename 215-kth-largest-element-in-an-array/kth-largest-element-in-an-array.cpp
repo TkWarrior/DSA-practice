e06204d671ed969568c95701a2662aa8163f5f06
TkWarrior <147327168+TkWarrior@users.noreply.github.com>
2025-07-08 13:30:39 +0530

    Time: 46 ms (12.31%) | Memory: 61.6 MB (44.77%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,20 +1,35 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int>pq;
+        //Approach 1-  Using Max Heap
+        // priority_queue<int>pq;
         
-        int n = nums.size();
+        // int n = nums.size();
+        
+        // whenever we add element in the priority queue it got added in the maxHeap order like in the below case it will be formed as [6,5,4,1,2,3]
+        // for(int i = 0 ; i<n ; i++){
+        //     pq.push(nums[i]);
+        // }
+        
+        // root element will get deleted now second largest element will take its place beacause internally it is implementing the maxHeap function by dafult
+       
+    //     for(int i = 1 ; i<k ;i++){
+    //           pq.pop();
+    //     }
+    
+    //    return pq.top();
 
-        for(int i = 0 ; i<n ; i++){
-            pq.push(nums[i]);
+        priority_queue<int , vector<int>, greater<int>>min_pq;
+
+        // we'll create a minHeap of size k
+        for(auto x:nums){
+            min_pq.push(x);
+            // as soon as the size of priority queue exceeds k pop the root element
+            if(min_pq.size()>k){
+                min_pq.pop();
+            }
         }
-        // root element will get deleted now second largest element will take its place
-        for(int i = 1 ; i<k ;i++){
-              pq.pop();
-        }
-      
 
-        return pq.top();
-
+        return min_pq.top();
     }
 };
