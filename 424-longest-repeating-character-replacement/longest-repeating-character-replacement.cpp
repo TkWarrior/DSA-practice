4177c588d4cdc5612487b979aa046ab0c64b2524
TkWarrior <147327168+TkWarrior@users.noreply.github.com>
2025-07-13 18:57:44 +0530

    Time: 14 ms (19.61%) | Memory: 11.1 MB (7.53%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,51 +1,21 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-
-        //Brute force
-        // int max_len = 0;
-        // int len = 0;
-        // int n = s.length();
-        // int max_freq = 0;
-        // for(int i = 0 ; i<n ; i++){
-        //     max_freq = 0 ;
-        //     int hash[26] = {};
-        //     for(int j = i ; j<n ; j++){
-        //         hash[s[j]-'A']++;
-        //         max_freq = max(max_freq,hash[s[j]-'A']);
-        //         len = j-i+1;
-        //         //no of conversions we have to do
-        //         int conversions = len - max_freq;
-        //         if(conversions<=k){
-        //             max_len = max(max_len,len);
-        //         }
-        //         else{
-        //             break;
-        //         }
-        //     }
-        // }
-        // return max_len;
-
-        /* Size of the window will be the no of conversions <= k
-        if it is greater than k then we will trim down the window size from the left
-        */
-        int l = 0 , r = 0;
-        int max_len = 0;
+        int l = 0 ; 
+        int r = 0 ; 
         int n = s.length();
-        unordered_map<int,int>hashmap;
+        unordered_map<char,int>umap;
         int max_freq = 0;
-        while(r<n){
-            hashmap[s[r]-'A']++;
-            max_freq = max(max_freq,hashmap[s[r]-'A']);
-            if((r-l+1)-max_freq > k){
-               hashmap[s[l]-'A']--;
-               
+        int max_len = 0;
+        while(r<n ){
+             umap[s[r]]++;
+             max_freq = max(max_freq,umap[s[r]]);
+             if((r-l+1)-max_freq > k){
+                umap[s[l]]--;
                 l++;
-            }
-            if((r-l+1)-max_freq<=k){
-                max_len = max(max_len,(r-l+1));
-            }
-            r++;
+             }
+             max_len = max(max_len , (r-l+1));
+             r++ ; 
         }
         return max_len;
     }
