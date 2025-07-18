2d469c160d225f9b15513723e9fbcafffec9ed64
TkWarrior <147327168+TkWarrior@users.noreply.github.com>
2025-07-18 18:17:46 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (46.12%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>>ans;
+   
+    void solve(vector<vector<int>>&ans ,vector<int>& candidates,vector<int>&res , int ind , int target){
+        
+        if(ind == candidates.size()){
+            if(target == 0){
+                ans.push_back(res);
+            }
+            return;
+        }
+       
+         res.push_back(candidates[ind]);
+         if(candidates[ind]<=target){
+           
+             solve(ans , candidates , res , ind , target-candidates[ind]);
+         }
+       
+
+        res.pop_back();
+      
+        solve(ans , candidates , res , ind + 1 , target);
+
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        int ind = 0 ; 
+        vector<int>res;
+
+        solve(ans , candidates,res , ind , target );
+        return ans;
+    }
+};
