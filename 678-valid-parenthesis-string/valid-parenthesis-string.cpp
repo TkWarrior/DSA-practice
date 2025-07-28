2866e8c0dbeb8ad60bdd25bb5303845728afedfc
TkWarrior <147327168+TkWarrior@users.noreply.github.com>
2025-07-28 18:11:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (27.89%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int t[101][201];
+    bool solve(int ind , int count ,  string&s){
+        if(ind==s.size()){
+            return count==0;      
+        }
+         if(t[ind][count+100] != -1){
+            return t[ind][count+100];
+        }
+        bool isValid = false;
+       
+        if(s[ind]=='*'){
+            // trying all the possiblity  or '*'='' or '*' = ')'
+            //if '*' = '('
+            isValid = isValid|solve(ind+1 , count+1 , s);
+            //if '*' = ''
+            isValid = isValid|solve(ind+1 , count , s);
+            //if '*' = ')'
+            if(count>0){
+                isValid = isValid|solve(ind+1 , count-1 , s);
+            }
+        }
+       else if(s[ind]=='('){
+            // false or true will give true
+            isValid = isValid|solve(ind+1 , count+1 , s);
+       }
+       else {
+            if(count>0){
+                 isValid = isValid|solve(ind+1 , count-1 , s);
+            }
+           
+       }
+      
+        return t[ind][count+100] = isValid;
+    }
+
+    bool checkValidString(string s) {
+       int count = 0 ; 
+       memset(t,-1,sizeof(t));
+       return solve(0 , count , s );
+    }
+};
