0f10d6912ba56b67cd3a1ec4565654b1faba1bb1
TkWarrior <147327168+TkWarrior@users.noreply.github.com>
2025-08-14 01:00:26 +0530

    Time: 5 ms (29.80%) | Memory: 9.8 MB (48.30%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        map<char,int>map1;
-        map<char,int>map2;
-
-        if(s.length()!=t.length()){
-            return false;
-        }
-
-        for(int i = 0 ; i<s.length() ; i++){
-            map1[s[i]]++;
-            map2[t[i]]++;
-        }
         
-        for(int i = 0 ; i<s.length() ; i++){
-            if(map1[s[i]] != map2[s[i]]){
+       map<char , int>mp1; 
+       map<char , int>mp2;
+       
+        if(s.length()!=t.length()) return false;
+
+       for(int i = 0 ; i<s.length() ; i++){
+            mp1[s[i]]++;
+            mp2[t[i]]++;
+       }
+
+       for(int i = 0 ; i<s.length() ; i++){
+            if(mp1[s[i]] != mp2[s[i]]){
                 return false;
             }
-        }
-        return true;
+       }
+
+       return true;
     }
 };
