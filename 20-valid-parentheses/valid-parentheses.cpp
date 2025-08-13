cb1f00a4948d843a2cf4e0fcf22b64e63f8869d7
TkWarrior <147327168+TkWarrior@users.noreply.github.com>
2025-08-14 04:50:15 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (15.01%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     bool isValid(string s) {
-       unordered_map<char,char>umap = {{')','('},{'}','{'},{']','['}};
-       stack<char>st;
-       for(char ch:s){
-            if(umap.find(ch)==umap.end()){
-                st.push(ch);
+        unordered_map<char ,char>umap = { {')','('} , {']','['},{'}','{'} };
+        stack<char>st;
+        
+        for(char c:s){
+            if(umap.find(c)==umap.end()){
+                st.push(c);
             }
-            else if(!st.empty() && umap[ch]==st.top()){
+
+            else if(!st.empty() && umap[c]==st.top()){
                 st.pop();
             }
             else{
                 return false;
             }
-       }
+           
+        }
+
        return st.empty();
     }
 };
