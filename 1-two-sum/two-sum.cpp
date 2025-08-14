ae43f02f9ddf4ea63be135ab805df45baf0c60bd
TkWarrior <147327168+TkWarrior@users.noreply.github.com>
2025-08-14 13:48:39 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (50.34%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-       //solution using hash map
+       int n = nums.size();
+        unordered_map<int,int>umap;
+       int key = 0 ;
 
-       //in this approach we've used hash map store the element with it's corresponding indices
-       //after that we have checked the key (target - nums[i]) present in the map and if it is present then we will return the corressponding value associated with it and the iteratator which is being currently pointed in the array
-       unordered_map<int,int>umap;
-        for(int i = 0 ; i<nums.size() ; i++){
-            int key = target - nums[i];
-            //if key is present 
+       for(int i = 0 ; i<n ; i++){
+           
+            key = target - nums[i];
+
             if(umap.find(key)!=umap.end()){
                 return {umap[key] , i};
             }
+
             umap[nums[i]] = i;
-        }
-        return {-1,-1};
+       }
+
+       return {};
+      
     }
 };
