8dc91f32f50f69c9c1d7eed4d550f82148425b98
TkWarrior <147327168+TkWarrior@users.noreply.github.com>
2025-09-13 22:16:26 +0530

    Time: 0 ms (100.00%) | Memory: 23.3 MB (5.71%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -13,31 +13,21 @@ class Solution {
 public:
 
     int height(TreeNode* node){
-        if(node==NULL){
-            return 0;
-        }
+       if(node == NULL) return 0;
 
-        int left = height(node->left);
-        int right = height(node->right);
+       int left = height(node->left);
+        if(left == -1) return -1;
+       int right = height(node->right);
+       if(right == -1) return -1;
 
-        return 1+max(left,right);
+       if(abs(left-right)>1) return -1;// main braking condition
+
+       return 1+max(left , right);
+
     }
     
     bool isBalanced(TreeNode* root) {
         
-        // if the height diff b/w left and right subtree is <=1 then it is a balanced bibary tree
-        if(root==NULL){
-            return true;
-        }
-        int leftHeight = 0 ;
-        int rightHeight = 0 ;
-        if(root->left!=NULL){
-             leftHeight = height(root->left);
-        }
-        if(root->right!=NULL){
-             rightHeight = height(root->right);
-        }
-      // we've to check balanced condition at every level of tree root , left subtree , and right subtree
-        return abs(rightHeight-leftHeight)<=1 && isBalanced(root->left) &&   isBalanced(root->right);
+        return height(root)!=-1;
     }
 };
