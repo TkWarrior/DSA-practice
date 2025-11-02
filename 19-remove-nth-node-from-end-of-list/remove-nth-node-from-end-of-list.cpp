0ffa352aa03daf6280f08896f64ab59f357aef4f
TkWarrior <147327168+TkWarrior@users.noreply.github.com>
2025-11-03 00:50:29 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (25.28%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -11,50 +11,30 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* l1 = head;
-        int count = 0;
-       
-       //Edge cases
-        if(head==NULL || head->next==NULL){
-            return NULL;
+        ListNode* temp = head ; 
+        int count = 0 ; 
+        if(head == NULL ){
+            return NULL ; 
         }
-        
-        //we will traverse the linked list and will count the no of nodes
-       while(l1!=NULL ){
-            count++;
-            l1 = l1->next;
-            
-       }
-        
-        // if the number of nodes is equal to element's position which has to be deleted
-        // it means it is pointed towards head of the linked list
-        if(count==n){
-            //we will Node pointer to the head->next
-            ListNode* newHead = head->next;
-            //then we will delete the head
-            // delete head;
-            // will return the new head
-            return newHead;
+        while(temp!=NULL){
+            count++ ;
+            temp = temp->next ; 
         }
-      
-       int res = count-n;
-       l1 = head;
-       while(l1!=NULL ){
-           res--;
-           if(res==0){
-                break;
-           }
-          
-           l1 = l1->next;
+
+        temp = head ; 
+
+        int rem = count-n ; 
+        if(rem == 0){
+            return head->next ; 
         }
-        ListNode* deleteNode = l1->next;
-        if(deleteNode){
-             
-       
-         l1->next = deleteNode->next;
-        //  delete deleteNode;
+        for(int i=0 ; i<rem-1 ; i++){
+            temp = temp->next ; 
         }
-       
-        return head;
+
+        ListNode*delNode = temp->next ; 
+
+        temp->next = delNode->next;
+
+        return head ; 
     }
-};
+};  
