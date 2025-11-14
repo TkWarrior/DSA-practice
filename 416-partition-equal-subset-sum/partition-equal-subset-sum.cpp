9d25018f0e76db3684618a01b5f81a22ce8025b6
TkWarrior <147327168+TkWarrior@users.noreply.github.com>
2025-11-14 11:11:13 +0530

    Time: 59 ms (89.35%) | Memory: 15.3 MB (76.90%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,43 +1,40 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        int sum = 0 ; 
-        int n =  nums.size();
+        int n = nums.size();
 
-        for(int i = 0; i<n ; i++){
+        int sum = 0;
+
+        for(int i = 0 ; i<n ; i++){
             sum += nums[i];
         }
-        if(sum%2 != 0 ){
-            return false;
-        }
-        int target = sum/2 ; 
-        int dp[n+1][target+1];
-        // initialization
+
+        if(sum%2!=0) return false ;
+        
+        int half = sum/2 ; 
+
+        bool dp[n+1][half+1]; // since return type is bool
 
         for(int i = 0 ; i<n+1 ; i++){
-            for(int j= 0 ; j<target+1 ; j++){
-                if(i==0 ){
+            for(int j = 0 ; j<half+1 ; j++){
+                if(i==0){
                     dp[i][j] = false;
-                }
-                if(j==0){
-                    dp[i][j] = true;
+                }if(j==0){
+                    dp[i][j] = true ; 
                 }
             }
         }
 
         for(int i = 1 ; i<n+1 ; i++){
-            for(int j= 1 ; j<target+1 ; j++){
+            for(int j = 1 ; j<half+1 ; j++){
                 if(nums[i-1]<=j){
-                    dp[i][j] = (dp[i-1][j-nums[i-1]]) || (dp[i-1][j]) ; 
-                }
-                else{
+                    dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
+                }else{
                     dp[i][j] = dp[i-1][j];
                 }
             }
         }
 
-        return dp[n][target];
-
-    
+        return dp[n][half];
     }
 };
