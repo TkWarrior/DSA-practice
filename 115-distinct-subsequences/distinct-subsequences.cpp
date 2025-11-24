3a9cf83ba7d4afed481f046ba30799f8096f334e
TkWarrior <147327168+TkWarrior@users.noreply.github.com>
2025-11-24 11:45:25 +0530

    Time: 30 ms (56.56%) | Memory: 44.3 MB (21.04%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int m = s.size() ; 
+        int n = t.size() ;
+        vector<vector<unsigned long long>>dp(m+1 , vector<unsigned long long>(n+1));
+
+        for(int i = 0 ; i<m+1 ; i++){
+            for(int j = 0 ; j<n+1 ; j++){
+                if(i==0){
+                    dp[i][j] = 0;
+                }
+                if(j==0){
+                    dp[i][j] = 1 ;
+                }
+            }
+        }
+        
+        for(int i = 1 ; i<m+1 ; i++){
+            for(int j = 1 ; j<n+1 ; j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }        
+        }
+
+        return dp[m][n];
+    }
+};
