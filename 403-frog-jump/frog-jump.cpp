77232e36d46a81a02b1943054ef46c856f6ba4bb
TkWarrior <147327168+TkWarrior@users.noreply.github.com>
2025-11-25 13:38:16 +0530

    Time: 58 ms (82.88%) | Memory: 34.3 MB (66.82%) - LeetSync

--- 403-frog-jump/frog-jump.cpp
+++ 403-frog-jump/frog-jump.cpp
@@ -8,6 +8,15 @@ public:
         if(dp[cs_i][pj]!=-1) return dp[cs_i][pj];
 
         bool reach = false ;
+        // on every next jump we have 2 possiblities either to jump forward to 
+        // 1. next jump = previous jump - 1  (k-1)
+        // 2.  or next jump = previous jump (k)
+        // 3 or next jump = previous jum+1 (k+1)
+
+        // the next stone can be found in anyone of the above jumps
+        // if the next stone is present in the map then we can do the recursion call otherwise explore other possiblity
+        // expote all the above 3 possiblities using recursion
+
         for(int next_j=pj-1 ; next_j <= pj+1;next_j++){
             if(next_j > 0){
                 int next_stone = stones[cs_i]+next_j ;
