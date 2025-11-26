b8d9fdb2fbed353a2c43b15f9f398650080b6c93
TkWarrior <147327168+TkWarrior@users.noreply.github.com>
2025-11-26 12:29:54 +0530

    Time: 14 ms (29.25%) | Memory: 26.1 MB (47.52%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,38 +1,40 @@
 class Solution {
 public:
-    int findMax(vector<int>&nums){
-        int maxi = INT_MIN;
-        for(int i = 0 ; i<nums.size() ; i++){
-            maxi = max(maxi,nums[i]);
+    
+    double calculateThreshold(vector<int>& nums , int mid){
+        int n = nums.size() ; 
+        double sum = 0 ; 
+        for(int i = 0 ; i<n ; i++){
+            sum += ceil(double(nums[i])/double(mid));
         }
-        return maxi;
-    }
 
-    int calculateThreshold(vector<int>& nums,int mid){
-       int totalThreshold = 0;
-       for(int i = 0 ; i<nums.size() ; i++){
-            totalThreshold += ceil((double)nums[i]/(double)mid);
-       }
-       return totalThreshold;
+        return sum ;
     }
 
     int smallestDivisor(vector<int>& nums, int threshold) {
-        //similar to koko eating banana problem
-        int low = 1;
-        int high = findMax(nums);
-        int ans  = INT_MAX;
-        int mid;
-        while(low<=high){
-            mid = (low+high)/2;
-            int totalThreshold = calculateThreshold(nums,mid);
-            if(totalThreshold<=threshold){
-                ans = mid;
-                high = mid-1;
-            }
-            else{
-                low = mid+1;
+        int n = nums.size() ; 
+        int maxi = INT_MIN ;
+        
+        for(int i = 0 ; i<n ; i++){
+            maxi = max(maxi , nums[i]);
+        }
+
+        int s = 1 ; 
+        int e = maxi ; 
+        int ans = INT_MAX ;
+      
+        while(s<=e){
+            int mid = (s+e)/2;
+            // as given in the question
+            if(calculateThreshold(nums , mid) <= threshold){
+                ans = mid ; //  store the value in the answer
+                e = mid - 1 ;
+            }else{
+                s = mid + 1 ;
             }
         }
-        return ans;
+
+        return ans ; 
+        
     }
 };
