979cef79281e03438d5c4bad913f861afff81850
TkWarrior <147327168+TkWarrior@users.noreply.github.com>
2025-11-26 13:28:40 +0530

    Time: 6 ms (78.14%) | Memory: 23 MB (44.80%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
-
+    // Binary search on answer
     double calculateHour(vector<int>& piles , int mid){
        
         double sum = 0 ; 
         int n = piles.size() ; 
        
         for(int i = 0 ; i<n ; i++){
+            // If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour. - through this statement of the question we can interpret that we've take the ceil value
             sum += ceil(double(piles[i])/double(mid));
         }
 
@@ -14,7 +15,7 @@ public:
     }
     int minEatingSpeed(vector<int>& piles, int h) {
      
-        int maxi = INT_MAX ; 
+        int maxi = INT_MIN ; 
         int n = piles.size() ; 
         
         for(int i = 0 ; i<n ; i++){
@@ -26,6 +27,7 @@ public:
         int ans = INT_MAX;
         while(s<=e){
             int mid = (s+e)/2;
+            // before the guard returns
             if(calculateHour(piles , mid)<=h){
                 ans = mid ;
                 e = mid-1;
