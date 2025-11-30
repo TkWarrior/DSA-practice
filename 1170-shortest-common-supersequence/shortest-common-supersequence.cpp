2659d068f02499015defbf50b764adf7d3e18bc1
TkWarrior <147327168+TkWarrior@users.noreply.github.com>
2025-11-30 11:31:07 +0530

    Time: 28 ms (15.63%) | Memory: 13 MB (97.91%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -1,66 +1,52 @@
 class Solution {
 public:
-
-    string LCS(string&str1, string&str2,int m , int n ){
-        // vector<vector<int>>dp(m+1 , vector<int>(n+1));
-        vector<vector<int>>dp(m+1,vector<int>(n+1)); 
-        string s = "";
-        for(int i = 0 ; i<m+1 ; i++){
-            for(int j = 0 ; j<n+1 ; j++){
-                if(i==0 || j==0){
-                    dp[i][j] = 0 ;
-                }
-            }
+     int dp[1001][1001];
+    int lcs(string&str1 , string&str2 , int m , int n){
+        if(m==0 || n==0) return 0 ;
+        if(dp[m][n] != -1) return dp[m][n];
+        if(str1[m-1]==str2[n-1]){
+            return dp[m][n] = 1 + lcs(str1 , str2 , m-1 ,n-1);
         }
 
-        for(int i = 1 ; i<m+1 ; i++){
-            for(int j = 1 ; j<n+1 ; j++){
-                if(str1[i-1]==str2[j-1]){
-                  
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                }else{
-                    dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
-                }
-            }
-        }
-
-       
-        int i = m , j = n ; 
-
-        while(i>0 && j>0){
-            if(str1[i-1]==str2[j-1]){
-                s.push_back(str1[i-1]);// store the common character
-                i--;
-                j--;
-            }
-            else{
-                if(dp[i-1][j]>dp[i][j-1]){
-                    s.push_back(str1[i-1]); // store the maximum character
-                    i--;
-                }else{
-                    s.push_back(str2[j-1]); // store the maximum character
-                    j--; 
-                }
-            }  
-        }
-
-        while(i>0){
-            s.push_back(str1[i-1]);
-            i--;
-        }
-
-        while(j>0){
-            s.push_back(str2[j-1]);
-            j--;
-        }
-        return s ; 
-
+        return dp[m][n] = max(lcs(str1 , str2 , m-1 ,n) , lcs(str1 ,str2 ,m , n-1));
     }
     string shortestCommonSupersequence(string str1, string str2) {
-        int m = str1.length();
-        int n = str2.length() ; 
-        string s = LCS(str1 , str2 , m , n );
-        reverse(s.begin() , s.end());
-        return s;  
+         int m = str1.size() ; 
+         int n = str2.size() ; 
+         memset(dp , -1 , sizeof(dp));
+         int LCS = lcs(str1 , str2 , m , n );
+         int i = m ;
+         int j = n ;
+         string res = "" ;
+
+         while(i>0 && j>0){
+            if(str1[i-1] == str2[j-1]) {
+                res.push_back(str1[i-1]);
+                i--;
+                j--;
+            }else{
+                if(dp[i-1][j] > dp[i][j-1]){
+                    res.push_back(str1[i-1]);
+                    i--;
+                }else{
+                    res.push_back(str2[j-1]);
+                    j--;
+                }
+            }
+         }
+
+         while(i>0){
+            res.push_back(str1[i-1]);
+            i-- ; 
+         }
+
+         while(j>0){
+            res.push_back(str2[j-1]);
+            j--;
+         }
+
+         reverse(res.begin() ,res.end());
+
+         return res ; 
     }
 };
