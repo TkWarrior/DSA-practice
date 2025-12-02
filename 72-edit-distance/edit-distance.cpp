3535fb2091dd20ac61f4c522b5fa47e60243280d
TkWarrior <147327168+TkWarrior@users.noreply.github.com>
2025-12-02 11:27:27 +0530

    Time: 1 ms (95.40%) | Memory: 13 MB (79.63%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
-    int dp[501][501];
-    int minOp(string&word1 , string&word2 , int m , int n){
-        if(m==0){
-            return n;
-        }
-         if(n==0){
-            return m;
-        }
-
-        if(dp[m][n]!=-1) return dp[m][n];
-       
-        if(word1[m-1]==word2[n-1]){
-            return dp[m][n] = minOp(word1 , word2 , m-1 ,n-1);
-        }
-        //minimum between insert delete and insert
-        int min1 = min(1+minOp(word1 , word2 ,m , n-1) , 1+minOp(word1 ,word2 ,m-1 ,n));
-
-        // minimum betweern replace , insert and delete
-        return dp[m][n] = min(min1 , 1+minOp(word1 , word2 ,m-1 , n-1));
-    }
+   
     int minDistance(string word1, string word2) {
-        int m = word1.size() ; 
-        int n = word2.size() ; 
-        memset(dp , -1 , sizeof(dp));
-        return  minOp(word1 , word2 , m , n ); 
+       int m = word1.size() ;
+       int n = word2.size() ; 
 
+       vector<vector<int>>dp(m+1 ,vector<int>(n+1));
+
+       for(int i= 0 ; i<m+1 ; i++){
+            for(int j = 0 ; j<n+1 ; j++){
+                if(i==0){
+                    dp[0][j] = j;
+                }if(j==0){
+                    dp[i][0] = i;
+                }
+            }
+       }
+
+       for(int i=1 ; i<m+1 ; i++){
+            for(int j = 1 ; j<n+1 ; j++){
+                // replace , insert , delete
+                if(word1[i-1]==word2[j-1]){
+                    dp[i][j] = dp[i-1][j-1]; // ignore
+                }else{
+                    dp[i][j] = 1 + min( min(dp[i-1][j] , dp[i][j-1]) , dp[i-1][j-1]);
+                }
+            }
+       }
+
+       return dp[m][n];
     }
 };
