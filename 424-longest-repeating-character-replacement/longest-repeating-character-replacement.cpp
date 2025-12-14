8786c7452cf88789e81e2c986d951d0bfee8c977
TkWarrior <147327168+TkWarrior@users.noreply.github.com>
2025-12-15 00:44:54 +0530

    Time: 15 ms (17.65%) | Memory: 11.2 MB (7.80%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -2,24 +2,25 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         int l = 0 ;
-        int r = 0 ;
-        unordered_map<char,int>umap;
+        int r = 0 ; 
+        int n = s.size() ; 
+        unordered_map<char ,int>umap ; 
         int max_freq = 0 ; 
-      
         int max_len = 0 ;
-        while(r<s.length()){
+      
+        while(r<n){
             umap[s[r]]++;
             max_freq = max(max_freq , umap[s[r]]);
 
-            if((r-l+1) - max_freq>k){
+            if((r-l+1)-max_freq > k){
                 umap[s[l]]--;
-                l++;
+                l++ ;
             }
             max_len = max(max_len , r-l+1);
-
             r++;
         }
 
-        return max_len;
+        return max_len ; 
+        
     }
 };
