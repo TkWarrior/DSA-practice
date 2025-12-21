77fc38f9db774394259a2b58bf52e41f13888bff
TkWarrior <147327168+TkWarrior@users.noreply.github.com>
2025-12-22 00:34:21 +0530

    Time: 0 ms (100.00%) | Memory: 26.1 MB (91.67%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int low = 0;
-        int n = nums.size();
-        int high = n-1;
-        int mid;
-       
+        // odd length array will always contain one single element 
+        // pair of duplicate elements will be of even and odd indices
+
+        int n = nums.size() ;
         if(n==1){
             return nums[0];
         }
@@ -15,19 +14,22 @@ public:
         if(nums[n-2]!=nums[n-1]){
             return nums[n-1];
         }
-        
-        while(low<=high){
-            mid = (low+high)/2;
-            if(nums[mid]!=nums[mid+1] && nums[mid-1]!=nums[mid]){
-                break;
+
+        int s = 0 ;
+        int e = n-1 ; 
+        int mid;
+        while(s<=e){
+            mid = (s+e)/2;
+            if(nums[mid-1]!=nums[mid] && nums[mid+1]!=nums[mid]){
+                break ;
             }
-            if((mid%2==0 && nums[mid]==nums[mid+1]) || (mid%2==1 && nums[mid-1]==nums[mid])){
-                low = mid+1;
-            }
-            else{
-                high = mid-1;
+            else if(mid%2==0 && nums[mid+1] == nums[mid] || mid%2 == 1 && nums[mid-1]==nums[mid]){
+                s = mid+1;
+            }else{
+                e = mid - 1 ; 
             }
         }
+
         return nums[mid];
     }
 };
