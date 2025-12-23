607150bdb13f8c18e0c81435dd372b739e9d8c87
TkWarrior <147327168+TkWarrior@users.noreply.github.com>
2025-12-24 00:06:21 +0530

    Time: 13 ms (96.96%) | Memory: 70.5 MB (44.79%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,66 +1,53 @@
 class Solution {
 public:
-// Brute Force Apprroach
-    int minimum(vector<int>&weights){
-        int mini = INT_MAX;
-        for(int i = 0 ; i<weights.size() ; i++){
-            mini = min(mini,weights[i]);
-        }
-        return mini;
-    }
-    int maximum(vector<int>&weights){
-        int maxi = INT_MIN;
-        for(int i = 0 ; i<weights.size() ; i++){
-            maxi = max(maxi,weights[i]);
-        }
-        return maxi;
-    }
-    bool possible(vector<int>& bloomDay,int mid , int m ,int k){
-        int count = 0;
-        int noOfBouque = 0;
-        for(int j = 0 ; j<bloomDay.size() ; j++){
-            if(bloomDay[j]<=mid){
-                count++;
-            }
-            else{
-                noOfBouque+=count/k;
-                count = 0;
-            }
-        }
-        noOfBouque+=count/k;
-        if(noOfBouque>=m){
-            return true;
-        }
-        return false;
-       
-    }
-    int minDays(vector<int>& bloomDay, long long m, long long k) {
-        
+    int countBouquet(vector<int>&bloomDay , int mid , int k){
         int n = bloomDay.size();
-        int low = minimum(bloomDay);
-        int high = maximum(bloomDay);
-        long long prod = m*k;
-        if(n<prod){
-            return -1;
+        int count = 0 ;
+        int no_of_bouque = 0 ;
+        for(int i = 0 ; i<n ; i++){
+            if(bloomDay[i]<=mid){
+                count++;
+            }else{
+                no_of_bouque += count/k ;
+                count = 0 ;
+            }
         }
-        //this approach will give TLE so we will now solve it using binary Search
-        // for(int i = minimum(bloomDay) ; i<=maximum(bloomDay) ; i++){
-        //     if(possible(bloomDay,i,m,k)){
-        //         return i;
-        //     }
-        // }
+
+        no_of_bouque += count/k;
+        return no_of_bouque;
+    }
+    int minDays(vector<int>& bloomDay, long long  m, long long k) {
+        int n = bloomDay.size() ; 
+
+        if(n < m*k) return -1 ;
+
+        int maxi  = INT_MIN ; 
+        int mini = INT_MAX ;
+
+        for(int i = 0 ; i<n ; i++){
+            maxi = max(maxi , bloomDay[i]);
+        }
+
+        for(int i = 0 ; i<n ; i++){
+            mini = min(mini , bloomDay[i]);
+        }
+
+        int s = mini ;
+        int e = maxi ;
         
-        //Optimized Approach
-        //finally able to optimize it by my self although for brute and to know the intution i have to gont through the video solution 
-        while(low<=high){
-            int mid = (low+high)/2;
-            if(possible(bloomDay,mid,m,k)){
-                high = mid-1;
-            }
-            else{
-                low = mid+1;
+
+        while(s<=e){
+            int mid = (s+e)/2;
+
+            if(countBouquet(bloomDay,mid ,k)>=m){
+              
+                e = mid-1;
+            }else{
+                s = mid+1;
             }
         }
-        return low;
+
+        return s ;
+
     }
 };
