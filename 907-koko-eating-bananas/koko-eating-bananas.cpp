fba6eacd0cca707a62580fc5c5f08f35a27e466c
TkWarrior <147327168+TkWarrior@users.noreply.github.com>
2025-12-23 23:05:03 +0530

    Time: 13 ms (25.16%) | Memory: 22.9 MB (76.22%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,41 +1,42 @@
 class Solution {
 public:
-    // Binary search on answer
-    double calculateHour(vector<int>& piles , int mid){
-       
-        double sum = 0 ; 
-        int n = piles.size() ; 
-       
-        for(int i = 0 ; i<n ; i++){
-            // If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour. - through this statement of the question we can interpret that we've take the ceil value
-            sum += ceil(double(piles[i])/double(mid));
+   
+    double calcK(vector<int>&piles ,int mid , int n, int h){
+        double k = 0;
+        
+        for(int i : piles){
+            k += ceil(double(i)/double(mid));
         }
 
-        return sum ;
+        return k ;
     }
+
     int minEatingSpeed(vector<int>& piles, int h) {
      
-        int maxi = INT_MIN ; 
-        int n = piles.size() ; 
-        
-        for(int i = 0 ; i<n ; i++){
-            maxi = max(maxi , piles[i]);
+       int n = piles.size();
+
+        int mini = INT_MAX ; 
+        int maxi = INT_MIN ;
+
+
+        for(int n : piles){
+            maxi = max(maxi , n);
         }
 
         int s = 0 ;
-        int e = maxi ; 
+        int e = maxi ;
         int ans = INT_MAX;
+        
         while(s<=e){
-            int mid = (s+e)/2;
-            // before the guard returns
-            if(calculateHour(piles , mid)<=h){
-                ans = mid ;
+            int mid = (s+e)/2 ; 
+            if(calcK(piles , mid ,n , h)<=h){
+                ans = mid ; 
                 e = mid-1;
             }else{
                 s = mid+1;
             }
         }
-
+    
         return ans ; 
     }
 };
