7f222aaedac94817a222121f77b6a3a622d8053c
TkWarrior <147327168+TkWarrior@users.noreply.github.com>
2025-12-25 17:47:28 +0530

    Time: 11 ms (65.80%) | Memory: 34.9 MB (84.47%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,49 +1,50 @@
 class Solution {
 public:
-   
-    bool isPossible(vector<int>& weights , int capacity , int days){
-            int load = 0; // iitially there is no load in the ship
-            int no_of_days = 1 ; // start from the day 1
 
-            for(int i=0 ; i<weights.size() ; i++){
-                // if ship exceeds the capacity
-                if(load + weights[i] > capacity){
-                    load = weights[i] ;// initialize the new weight from the next day
-                    no_of_days++ ; // start from the next day
-                }else{
-                    load += weights[i] ; // add weights within that day itself
-                } 
+    bool possible(vector<int>&weights , int mid , int days , int n ){
+        int load = 0;
+        int no_of_days = 1 ;
+
+        for(int i = 0 ; i<n ; i++){
+            if(load+weights[i]>mid){
+                load = weights[i];
+                no_of_days++;
+            }else{
+                load += weights[i] ;
             }
-
-            return no_of_days <= days ;
       
+        }
+
+        return no_of_days<=days ;
     }
-    
-    int shipWithinDays(vector<int>& weights, int days) {
+
+    int shipWithinDays(vector<int>& weights, int days ) {
        
+       int n = weights.size() ; 
+
+        int sum = accumulate(weights.begin() , weights.end() , 0);
+        cout<<sum ; 
         int maxi = INT_MIN ; 
-        int n = weights.size() ;
 
         for(int i = 0 ; i<n ; i++){
             maxi = max(maxi , weights[i]);
         }
 
-        int low = maxi ; 
-        int high = accumulate(weights.begin() , weights.end() ,0) ;
-        int ans = 0 ; 
+        int s = maxi; 
+        int e = sum ; 
+        int ans = -1;
 
-        while(low<=high){
-            int mid = (low+high)/2 ;
-            // if it is possible to ship the packageof of capacity mid 
-            if(isPossible(weights , mid , days)){
-                ans =  mid ; 
-                high = mid-1;
+        while(s<=e){
+            int mid = s+(e-s)/2 ;
+            cout<<mid<<endl; 
+            if(possible(weights , mid  , days , n)){
+                e = mid-1;
             }else{
-                low = mid+1;
+                s = mid+1;
             }
         }
-        cout<<ans ;
+
         
-       return ans;
+        return s ; 
     }
 };
