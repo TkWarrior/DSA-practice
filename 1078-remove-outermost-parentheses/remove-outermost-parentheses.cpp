0ff9ab9437860e056c465814bbdcc77a236520c2
TkWarrior <147327168+TkWarrior@users.noreply.github.com>
2026-01-03 21:09:47 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (23.53%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        string s1 = "";
+       
+        stack<char>st;
+        string res = "";
 
-        int count = 0 ;
-        vector<string>v1;
-        for(int i = 0 ; i<s.length() ; i++){
-            s1 = s1+s[i];
-            if(s[i]=='('){
-                count++;
+        for(char ch : s){
+            if(ch=='(' ){
+               if(!st.empty()){
+                    res += ch ; 
+               }
+               st.push(ch);
+            }else{ // ch = ')'
+                st.pop();
+                if(!st.empty()){
+                    res += ch ;
+                }     
             }
-            else{
-                count--;
-            }
-            if(count == 0){
-                v1.push_back(s1);
-                s1 = "";
-            }
+           
         }
 
-        string s2 = "";
-        for(int j = 0 ; j<v1.size() ; j++){
-            v1[j].erase(v1[j].begin());
-            v1[j].erase(v1[j].end()-1);
-            s2 =  s2+v1[j] ;
-        }
-        return s2;
+        return res ;
     }
 };
