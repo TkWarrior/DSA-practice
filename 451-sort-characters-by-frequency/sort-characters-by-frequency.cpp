aa7537c841d6949e0d13bcb3d7da81b45a61c478
TkWarrior <147327168+TkWarrior@users.noreply.github.com>
2026-01-05 01:21:48 +0530

    Time: 2 ms (61.67%) | Memory: 11.5 MB (24.79%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,51 +1,27 @@
 class Solution {
 public:
-    typedef pair<char,int>P;
     string frequencySort(string s) {
-        // learnt about lambda - in  this problem it is used for custom sorting 
-        // vector mein pair bhi store kara sakte hai like in this case we have used the pair of character and its frequency inside the vector
-        // vector<pair<char,int>>>vec(123)
-        vector<P>vec(123);
-        string result = "";
-        for(char &ch : s){
+       typedef pair<char , int>P ; 
+       vector<P>vec(123) ; 
+
+        for(char&ch :s){
             int freq = vec[ch].second;
-            vec[ch] = {ch,freq+1};
+            vec[ch] = {ch , freq+1};
         }
 
-        auto lambda = [&](P & p1,P & p2){
-            return p1.second>p2.second;
-        };
-        sort(vec.begin(),vec.end(),lambda);
-        
+        sort(vec.begin() , vec.end() ,[&](P & p1 , P & p2){
+            return p1.second > p2.second ;
+        });
+        string res = "";
+
         for(int i = 0 ; i<123 ; i++){
-            if(vec[i].second>0){
-                char ch = vec[i].first;
-                int freq = vec[i].second;
-                string temp = string(freq,ch);
-                result += temp;
-            }
+            if(vec[i].second > 0){
+                int freq = vec[i].second ; 
+                char ch = vec[i].first; 
+                res += string(freq , ch);
+            }       
         }
 
-        return result;
-        // int count = 0;
-        // int i = 0 ;
-        // string s1 = "";
-        // int maxi = -1;
-        // while(i<s.length()){
-        //     if(s[i]==s[i+1]){
-        //         count++;
-        //     }
-        //     maxi = max(count,maxi);
-        //     if(count>maxi){
-               
-        //         s1 = s1 + s[i];
-        //     }
-        //     else{
-        //         s1 = s[i] + s1;
-        //     }
-        //     i++;
-        // }
-        
-        // return s1;
+        return res ;
     }
 };
