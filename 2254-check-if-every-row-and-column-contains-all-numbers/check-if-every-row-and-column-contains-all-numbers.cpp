c53d75b57fbde7626412c3ff482d7294e14b5445
TkWarrior <147327168+TkWarrior@users.noreply.github.com>
2026-01-11 03:23:59 +0530

    Time: 143 ms (34.63%) | Memory: 94.7 MB (34.32%) - LeetSync

--- 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.cpp
+++ 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.cpp
@@ -5,18 +5,20 @@ public:
         int n = matrix.size() ;
         // every row and and its corresponding coloumn should not contain duplicate value
         for(int i = 0 ; i<n ; i++){
-            vector<bool>rowVisited(n+1 , false);
-            vector<bool>colVisited(n+1 , false);
+            // vector<bool>rowVisited(n+1 , false);
+            // vector<bool>colVisited(n+1 , false);
+            unordered_map<int,bool>mp1 ;
+            unordered_map<int,bool>mp2 ;
             for(int j = 0 ; j<n ; j++){
                 int rVal = matrix[i][j]; // matrix[0][1]
                 int cVal = matrix[j][i]; // matrix[1][0]
 
-                if(rowVisited[rVal]==true || colVisited[cVal]==true){
+                if(mp1[rVal]==true || mp2[cVal]==true){
                     //invalid matrix
                     return false;
                 }
-                rowVisited[rVal] = true ;
-                colVisited[cVal] = true ;
+                mp1[rVal] = true ;
+                mp2[cVal] = true ;
 
             }
         }
