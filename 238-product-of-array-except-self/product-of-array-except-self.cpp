8238e2c50fe743106cbfa413bfb35392c9c4aba7
TkWarrior <147327168+TkWarrior@users.noreply.github.com>
2026-01-11 00:50:21 +0530

    Time: 4 ms (21.61%) | Memory: 42.1 MB (25.16%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,53 +1,29 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-    //Brute force
-    //     vector<int>ans;
-    //     int curr_prod = 1;
-    //     int n = nums.size();
-    //     for(int i = 0 ; i<n ; i++){
-    //         curr_prod = 1;
-    //         for(int j = 0 ; j<n ; j++){
-    //             if(j==i) continue;
-    //             curr_prod = curr_prod*nums[j];
-    //         }
-    //         ans.push_back(curr_prod);
-    //     }
-    // return ans;
+        int n = nums.size();
 
-    //Optimal
-    int n = nums.size();
-    vector<int>left_prod(n);
-    vector<int>right_prod(n);
-   
-    int prod = 1;
-    //for left_prod array
-    left_prod[0] = 1;
-    // [1,1,2,6]
-    // [1*1 , 1*1,2*1,2*3]
-    
-    for(int i = 1 ; i<n ; i++){
-        prod = nums[i-1]*left_prod[i-1];
-        left_prod[i] = prod;
-    }
-    
-    // for right_prod array
-    // [24,12,4,1]
-    // [ 12*2,4*3 , 1*4,1]
-    
-    right_prod[n-1] = 1;
-    prod = 1;
+        //prefix[i]*suffix[i]
+        // at every index the product of prefix 0 to i-1 and suffix i+1 to n-1
 
-    for(int i = n-2 ; i>=0 ; i--){
-        prod = nums[i+1]*right_prod[i+1];
-        right_prod[i] = prod;
-    }
+        vector<int>prefix(n) ;
+        vector<int>suffix(n) ; 
+        prefix[0] = 1 ; 
+        suffix[n-1] = 1 ; 
+        vector<int>ans(n);
 
-    vector<int>ans(n);
-    for(int i = 0; i<n ; i++){
-        ans[i] =  left_prod[i]*right_prod[i];
+        for(int i = 1 ; i<n ; i++){
+            prefix[i] = prefix[i-1]*nums[i-1];
+        }
 
-    }
-    return ans;
+        for(int i = n-2 ; i>=0 ; i--){
+            suffix[i] = suffix[i+1]*nums[i+1];
+        }
+
+        for(int i = 0 ; i<n ; i++){
+            ans[i] = prefix[i]*suffix[i];
+        }
+
+        return ans ;
     }
 };
