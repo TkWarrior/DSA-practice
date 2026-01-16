dff856807628644a8fa9de383b060396d72a68bf
TkWarrior <147327168+TkWarrior@users.noreply.github.com>
2026-01-16 16:25:02 +0530

    Time: 0 ms (100.00%) | Memory: 26 MB (78.36%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,38 +1,36 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-       int total = 0 ;
-       int ind = 0 ;
-       int n = height.size();
-       int left_max = 0  ;
-       int right_max = 0;
-       int max_height = height[0];
+        int left_max = 0 ;
+        int right_max = 0 ;
+        int max_height = height[0];
+        int n = height.size();
+        int ind =0;
+        int water = 0 ;
 
-       for(int i = 1 ; i<n ; i++){
+        for(int i = 1 ; i<n ;i++){
             if(height[i]>max_height){
                 max_height = height[i];
-                ind = i ; 
-            } 
-       }
-       cout<<ind ;
-        // solving left part
-       for(int i = 0 ; i<ind ; i++){
+                ind = i;
+            }
+        }
+
+        for(int i =0 ; i<ind ; i++){
             if(left_max>height[i]){
-                total += left_max-height[i];
+                water += left_max-height[i];
             }else{
                 left_max = height[i];
             }
-       }
-       // solving for the right part
-
-       for(int i = n-1 ; i>ind ; i--){
+        }
+        
+        for(int i = n-1 ; i>ind ; i--){
             if(right_max>height[i]){
-                total += right_max-height[i];
+                water += right_max-height[i];
             }else{
                 right_max = height[i];
             }
-       }
+        }
 
-    return total ;
+        return water ;
     }
 };
