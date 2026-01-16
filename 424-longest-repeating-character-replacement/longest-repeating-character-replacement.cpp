d80adbbc1cbfd020e314db85f24919bf4d1be2dc
TkWarrior <147327168+TkWarrior@users.noreply.github.com>
2026-01-16 17:57:02 +0530

    Time: 7 ms (45.61%) | Memory: 11.1 MB (18.30%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
+        int n = s.size();
         int l = 0 ;
-        int r = 0 ; 
-        int n = s.size() ; 
-        unordered_map<char ,int>umap ; 
-        int max_freq = 0 ; 
-        int max_len = 0 ;
-      
-        while(r<n){
-            umap[s[r]]++;
-            max_freq = max(max_freq , umap[s[r]]);
+        int r = 0 ;
+        int max_length = 0;
+        unordered_map<char,int>freq;
+        int max_freq = 0 ;
+        int wl = 0 ; // window size 
+        int max_wl = 0 ;
 
-            if((r-l+1)-max_freq > k){
-                umap[s[l]]--;
-                l++ ;
+        while(r<n){
+            freq[s[r]]++;
+            max_freq = max(max_freq , freq[s[r]]);
+            wl = r-l+1;
+            while(wl-max_freq > k){ // keep shrinking the window until it beacame valid again
+                freq[s[l]]--;
+                l++;
+                wl = r-l+1 ; 
             }
-            max_len = max(max_len , r-l+1);
-            r++;
+            if(wl-max_freq <= k){ // only for valid
+                max_wl = max(wl , max_wl);
+                r++;
+            } 
         }
 
-        return max_len ; 
+        return max_wl ;
         
     }
 };
