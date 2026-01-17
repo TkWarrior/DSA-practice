3dec09835a1a003af71fdfe5d4f716e3c50937d8
TkWarrior <147327168+TkWarrior@users.noreply.github.com>
2026-01-17 15:24:09 +0530

    Time: 12 ms (7.95%) | Memory: 21.9 MB (98.47%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
-    bool isPalindrome(int l , int r , string s){
-       
+    bool checkPalindrome(int l , int r , string&s){
         while(l<r){
             if(s[l]!=s[r]){
-               return false ;
+                return false;
             }
             l++;
             r--;
         }
-
-        return true ;
-
+        return true;
     }
     bool validPalindrome(string s) {
         int n = s.size();
-        int l = 0 ; 
-        int r = n-1 ; 
-
+        int l = 0 ;
+        int r = n-1;
+      
         while(l<r){
             if(s[l]==s[r]){
                 l++;
                 r--;
-            }else{ // if mismatch we just need to figure which one character should we delete so that the string becomes palindrome
-                return isPalindrome(l+1 , r, s) || isPalindrome(l ,r-1, s);
+            }else{
+                return checkPalindrome(l+1 , r, s) || checkPalindrome(l , r-1, s);
             }
         }
 
