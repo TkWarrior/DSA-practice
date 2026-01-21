ef2c75dcb01435e07fd82b54cc11a924c8e034f1
Talibul Haque Khan <147327168+TkWarrior@users.noreply.github.com>
2026-01-21 23:30:09 +0530

    Time: 13 ms (88.72%) | Memory: 103 MB (67.74%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        //this question is similar to next greater element problem 
-        // first i will teaverse the array from the last 
-        // then i will store the indexes of next greater element in the stack
-
         int n = temperatures.size();
         vector<int>ans(n);
-        stack<int>st;
+        stack<int>st ; 
+
         for(int i = n-1 ; i>=0 ; i--){
             while(!st.empty() && temperatures[st.top()]<=temperatures[i]){
                 st.pop();
             }
             if(st.empty()){
-                ans[i] = 0;
-            }
-            else{
+                ans[i] = 0 ;
+            }else{
                 ans[i] = st.top() - i;
             }
-
             st.push(i);
         }
-        return ans;
+
+        return ans ;
+
+        return ans ;
+    
     }
 };
