d8d50da0e07c85b1ffb43c90b6f72df977478697
Talibul Haque Khan <147327168+TkWarrior@users.noreply.github.com>
2026-02-13 23:01:20 +0530

    Time: 0 ms (100.00%) | Memory: 79.5 MB (9.68%) - LeetSync

--- 445-add-two-numbers-ii/add-two-numbers-ii.cpp
+++ 445-add-two-numbers-ii/add-two-numbers-ii.cpp
@@ -10,34 +10,34 @@
  */
 class Solution {
 public:
-    ListNode* reverseLL(ListNode*head){
-        if(head==NULL || head->next==NULL){
-            return head ;
-        }
-        ListNode*last = reverseLL(head->next);
-        head->next->next = head ;
-        head->next = NULL ;
-
-        return last ; 
-    }
+    
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* L1 = reverseLL(l1);
-        ListNode* L2 = reverseLL(l2);
+        stack<int>s1 , s2 ;
 
-        ListNode* res = new ListNode(l1->val);
+        while(l1!=NULL){
+            s1.push(l1->val);
+            l1 = l1->next ;
+        }
+
+        while(l2!=NULL){
+            s2.push(l2->val);
+            l2 = l2->next ;
+        }
+
+        ListNode* res = new ListNode(s1.top());
         int sum = 0 ;
         int carry = 0 ;
 
-        while(L1!=NULL || L2!=NULL){
+        while(!s1.empty() || !s2.empty()){
 
-            if(L1!=NULL){
-                sum += L1->val;
-                L1 = L1->next ;
+            if(!s1.empty()){
+                sum += s1.top();
+                s1.pop() ;
             }
            
-            if(L2!=NULL){
-                sum += L2->val;
-                L2 = L2->next ;
+            if(!s2.empty()){
+                sum += s2.top();
+                s2.pop() ;
             }
 
             res->val = sum%10 ; 
