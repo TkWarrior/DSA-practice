ec24d175a122ed51f285b56082e096e846b7e29e
Talibul Haque Khan <147327168+TkWarrior@users.noreply.github.com>
2026-03-03 00:38:20 +0530

    Time: 7 ms (70.10%) | Memory: 23.1 MB (12.10%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,42 +1,40 @@
 class Solution {
 public:
-   
-    double calcK(vector<int>&piles ,int mid , int n, int h){
-        double k = 0;
+    double calcK(vector<int>& piles , int mid){
+        
+        double k = 0 ;
         
         for(int i : piles){
             k += ceil(double(i)/double(mid));
         }
 
-        return k ;
+        return k;
     }
-
     int minEatingSpeed(vector<int>& piles, int h) {
-     
-       int n = piles.size();
-
+        
+        int n = piles.size();
         int mini = INT_MAX ; 
         int maxi = INT_MIN ;
 
-
-        for(int n : piles){
-            maxi = max(maxi , n);
+        for(int i = 0 ; i<n ; i++){
+            maxi = max(maxi , piles[i]);
         }
 
         int s = 0 ;
         int e = maxi ;
-        int ans = INT_MAX;
+        int mid ;
+        int ans = maxi ;
         
         while(s<=e){
-            int mid = (s+e)/2 ; 
-            if(calcK(piles , mid ,n , h)<=h){
-                ans = mid ; 
+            mid = s + (e-s)/2;
+            if(calcK(piles ,mid)<=h){
+                ans = mid;
                 e = mid-1;
             }else{
                 s = mid+1;
             }
         }
-    
-        return ans ; 
+
+        return ans;
     }
 };
