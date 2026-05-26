49b33c78a4380aa8744be6529d26b7727492b62a
Talibul Haque Khan <147327168+TkWarrior@users.noreply.github.com>
2026-05-26 10:15:31 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (5.53%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<int>st ;
-        unordered_map<char , char>mp = {{')','('},{']','['},{'}','{'}};
-        
+        unordered_map<int,int>mp = { {')','('} , {'}' , '{'} , {']','['}};
+
         for(char&ch : s){
             if(st.empty()){
                 st.push(ch);
             }
-            else if(!st.empty() && mp[ch]==st.top()){
+            else if(!st.empty() && st.top()==mp[ch]){
                 st.pop();
             }
             else{
