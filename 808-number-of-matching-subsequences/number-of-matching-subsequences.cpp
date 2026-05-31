b1021846df2f29d317e583cee3dc1d7519584646
Talibul Haque Khan <147327168+TkWarrior@users.noreply.github.com>
2026-05-31 11:00:31 +0530

    Time: 116 ms (40.09%) | Memory: 55.3 MB (73.69%) - LeetSync

---
+++ 808-number-of-matching-subsequences/number-of-matching-subsequences.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool solve(string&s ,string&t ,map<char , vector<int>>&mp){
+        // int m = s.size();
+        int n = t.size();
+        int prev = -1;
+        
+        for(int i = 0 ; i<n ; i++){
+            if(mp.find(t[i])==mp.end()){
+                return false;
+            }
+            const vector<int>&ind = mp[t[i]];
+            auto it = upper_bound(begin(ind),end(ind),prev);
+
+            if(it==ind.end()){
+                return false;
+            }
+            prev = *it;
+                 
+        }
+
+        return true;
+    }
+    int numMatchingSubseq(string s, vector<string>& words) {
+        int cnt = 0;
+        map<char , vector<int>>mp ;
+        int m = s.size();
+
+        for(int i = 0 ; i<m ; i++){
+            mp[s[i]].push_back(i);
+        }
+
+        int size = words.size();
+
+        for(int i = 0 ; i<size ; i++){
+            if(solve(s , words[i], mp)){
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+};
