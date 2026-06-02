2b26202ae7e36f1c360e50dc74ac976eed40416f
Talibul Haque Khan <147327168+TkWarrior@users.noreply.github.com>
2026-06-02 07:28:12 +0530

    Time: 3 ms (62.17%) | Memory: 7.7 MB (65.20%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
-    int fib(int n) {
+    int dp[31][31];
+    int solve(int n ){
         if(n==0 || n==1){
             return n;
         }
+        if(dp[n][n]!=-1){
+            return dp[n][n];
+        }
+        return dp[n][n] = solve(n-1)+solve(n-2);
+    }
 
-        return fib(n-1)+fib(n-2);
+    int fib(int n) {
+        memset(dp,-1,sizeof(dp));
+        return solve(n);
     }
 };
