b9bbf48686532948c3a3abd17be25286ec6af2d0
Talibul Haque Khan <147327168+TkWarrior@users.noreply.github.com>
2026-06-30 22:18:40 +0530

    Time: 359 ms (25.28%) | Memory: 41.8 MB (31.64%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
     int dp[2501][2501];
-    int lis(vector<int>&nums , int prev_ind , int ind, int n){
-        if(ind>=n){
+    int lcs(vector<int>& nums ,int prev_ind , int ind , int n){
+        if(ind==n){
             return 0;
         }
-
+        int take = 0 ;
         if(prev_ind!=-1 && dp[prev_ind][ind]!=-1){
             return dp[prev_ind][ind];
         }
-        
-        int take = 0 ;
-
-        if(prev_ind==-1 || nums[prev_ind] < nums[ind]){
-           take = 1 + lis(nums , ind , ind+1 , n);
+        if(prev_ind==-1 || nums[prev_ind]<nums[ind]){
+            take = 1 + lcs(nums , ind , ind+1 , n);
         }
-        int skip = lis(nums , prev_ind , ind+1 , n);
-
+        int skip = lcs(nums,prev_ind , ind+1 , n);
         if(prev_ind!=-1){
             return dp[prev_ind][ind] = max(take , skip);
         }
-        
         return max(take , skip);
     }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        int prev_ind = -1 ;
         int ind = 0 ;
+        int prev_ind = -1;
         memset(dp , -1 ,sizeof(dp));
-        return lis(nums ,prev_ind , ind  , n);
+        return lcs(nums ,prev_ind , ind , n);
     }
 };
