bb04918455bbb4ce40b5cb64646bc8e961f34888
Talibul Haque Khan <147327168+TkWarrior@users.noreply.github.com>
2026-07-10 15:05:53 +0530

    Time: 339 ms (26.24%) | Memory: 41.6 MB (34.28%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
     int dp[2501][2501];
-    int lcs(vector<int>& nums ,int prev_ind , int ind , int n){
-        if(ind==n){
+    int solve(vector<int>& nums , int prev_ind , int curr_ind , int n){
+        if(curr_ind==n){
             return 0;
         }
+
+        if(prev_ind!=-1 && dp[prev_ind][curr_ind]!=-1){
+            return dp[prev_ind][curr_ind];
+        }
+
         int take = 0 ;
-        if(prev_ind!=-1 && dp[prev_ind][ind]!=-1){
-            return dp[prev_ind][ind];
+
+        if(prev_ind==-1 || nums[curr_ind]>nums[prev_ind]){
+            take =  1 + solve(nums , curr_ind , curr_ind+1 , n);
         }
-        if(prev_ind==-1 || nums[prev_ind]<nums[ind]){
-            take = 1 + lcs(nums , ind , ind+1 , n);
-        }
-        int skip = lcs(nums,prev_ind , ind+1 , n);
+
+        int skip = solve(nums , prev_ind , curr_ind+1 , n);
+
         if(prev_ind!=-1){
-            return dp[prev_ind][ind] = max(take , skip);
+            return dp[prev_ind][curr_ind] = max(take,skip);
         }
-        return max(take , skip);
+        return max(take,skip);
     }
     int lengthOfLIS(vector<int>& nums) {
+       
         int n = nums.size();
-        int ind = 0 ;
+        int curr_ind = 0 ;
         int prev_ind = -1;
-        memset(dp , -1 ,sizeof(dp));
-        return lcs(nums ,prev_ind , ind , n);
+        memset(dp,-1,sizeof(dp));
+        return solve(nums , prev_ind , curr_ind , n);
+        
     }
 };
