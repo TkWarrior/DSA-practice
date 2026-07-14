07549eae7f17062f20e05cedf46ae9a72350e13b
Talibul Haque Khan <147327168+TkWarrior@users.noreply.github.com>
2026-07-14 20:13:49 +0530

    Time: 3 ms (80.05%) | Memory: 19.6 MB (27.27%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -2,21 +2,22 @@ class Solution {
 public:
     int minSteps(string s, string t) {
         
-        unordered_map<char,int>mp;
+        vector<int>mp(26);
         int sum = 0;
 
         for(char c : s){
-            mp[c]++;
+            mp[c-'a']++;
         }
 
         for(char c : t){
-            mp[c]--;
+            mp[c-'a']--;
         }
 
-        for(auto it :mp){
-            if(it.second>0){
-                sum += it.second;
+        for(int i = 0 ; i<26 ; i++){
+            if(mp[i]>0){
+                sum += mp[i];
             }
+            
         }
 
         return abs(sum) ;
