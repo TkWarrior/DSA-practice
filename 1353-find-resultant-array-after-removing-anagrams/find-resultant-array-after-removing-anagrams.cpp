cd03a54d7fc1e2ba79aeedd828f04c9a8179aede
Talibul Haque Khan <147327168+TkWarrior@users.noreply.github.com>
2026-07-18 15:33:06 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (49.31%) - LeetSync

---
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<string> removeAnagrams(vector<string>& words) {
+        vector<string>ans ;
+
+        ans.push_back(words[0]);
+
+        for(int i = 1 ; i<words.size() ; i++){
+            string a = words[i];
+            sort(a.begin(),a.end());
+            string b = ans.back();
+            sort(b.begin(),b.end());
+            if(a!=b){
+                ans.push_back(words[i]);
+            }
+        }
+
+        return ans;
+
+       
+    }
+};
