9b088001d0ce6ee5bd59a1a21842cad4e4ca0d6d
Talibul Haque Khan <147327168+TkWarrior@users.noreply.github.com>
2026-07-21 11:57:15 +0530

    Time: 3 ms (39.77%) | Memory: 11.7 MB (73.17%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -1,34 +1,24 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
+       
         stack<int>st ;
         st.push(-1);
-        int res = 0 ;
+        int res = 0;
+
         for(int i = 0 ; i<s.length() ; i++){
             if(s[i]=='('){
                 st.push(i);
             }else{
                 st.pop();
-                if(st.empty() ){
+                if(st.empty()){
                     st.push(i);
                 }else{
-                    res = max(res , i - st.top()); 
+                    res = max(res , i-st.top());
                 }
             }
         }
-        return res ;
-        // int count = 0 ;
-        // unordered_map<char,char>umap = {{')','('}};
-        // stack<char>st ;
 
-        // for(int i = 1 ; i<s.length() ; i++){
-        //     st.push(s[i-1]);
-        //     if(!st.empty() && s[i]==')' && st.top() == '(' ){
-        //         count +=2 ; 
-        //         st.pop();
-        //     }
-        // }
-
-        // return count ;
+        return res;
     }
 };
