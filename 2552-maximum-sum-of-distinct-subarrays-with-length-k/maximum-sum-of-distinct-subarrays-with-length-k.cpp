bd85368756035f0fa5d5a2882da459fbf8ba5b0a
Talibul Haque Khan <147327168+TkWarrior@users.noreply.github.com>
2026-08-11 09:08:46 +0530

    Time: 131 ms (10.59%) | Memory: 99.6 MB (24.81%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
-        int i = 0 ;
-        int j = 0 ;
+        int i = 0 , j = 0 ;
+        unordered_set<int>st;
         int n = nums.size();
         long long s = 0 ;
         long long max_s = 0 ;
-        unordered_set<int>st ;
 
-        while(j<n){ 
-            
+        while(j<n){
+            s += nums[j];
             while(st.find(nums[j])!=st.end()){
                 s -= nums[i];
                 st.erase(nums[i]);
                 i++;
-            }
-            s += nums[j];
+            }  
             st.insert(nums[j]);
-            if(j-i+1 == k){
-                max_s = max(max_s , s);
-                cout<<max_s<<endl;
-                s -= nums[i];
+            if(j-i+1==k){
+                max_s = max(max_s ,s);
+                s -= nums[i];          
                 st.erase(nums[i]);
                 i++;
             }
-
             j++;
-            
         }
 
         return max_s;
