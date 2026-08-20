d88a9934bf7d07ba53c72ec820ea6b9feaa19169
Talibul Haque Khan <147327168+TkWarrior@users.noreply.github.com>
2026-08-20 13:49:26 +0530

    Time: 946 ms (5.00%) | Memory: 60.9 MB (61.52%) - LeetSync

--- 2478-longest-nice-subarray/longest-nice-subarray.cpp
+++ 2478-longest-nice-subarray/longest-nice-subarray.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
     int longestNiceSubarray(vector<int>& nums) {
-        // int i = 0 ;
-        // int j = 0 ;
-        // int n = nums.size();
-        // int mask = 0 ;
-        // int maxi = 1 ; 
+        int i = 0 ;
+        int j = 0 ;
+        int n = nums.size();
+        int mask = 0 ;
+        int maxi = 1 ; 
 
-        // while(j<n){
-        //     if((mask & nums[j]) == 0){
-        //         maxi = max(maxi , j-i+1);
-        //         cout<<j-i+1<<endl ;     
-        //     }else{
-        //         mask = 0 ;
-        //         i++;      
-        //     }
-        //     mask = mask | nums[j];
-        //     j++;
-        // }   
+        while(j<n){
+
+            while((mask & nums[j]) != 0){
+                mask = mask ^ nums[i];
+                i++;
+            }     
+            maxi = max(maxi , j-i+1);      
+            cout<<j-i+1<<endl ;          
+            mask = mask | nums[j];
+            j++;
+        }   
 
         // return maxi ;
 
-        int maxi = 1 ; 
-        int mask = 0 ;
-        int n = nums.size();
+        // int maxi = 1 ; 
+        // int mask = 0 ;
+        // int n = nums.size();
 
-        for(int i = 0 ; i<n ; i++){
-            mask = 0 ;
-            for(int j = i ; j<n ; j++){
-                if((mask&nums[j])!=0){
-                    break;
-                }
-                maxi = max(maxi , j-i+1);
-                mask |= nums[j];
-            }
-        }
+        // for(int i = 0 ; i<n ; i++){
+        //     mask = 0 ;
+        //     for(int j = i ; j<n ; j++){
+        //         if((mask&nums[j])!=0){
+        //             break;
+        //         }
+        //         maxi = max(maxi , j-i+1);
+        //         mask |= nums[j];
+        //     }
+        // }
 
         return maxi ;
     }
